e6a8b3680d2bc029095c7257c74a1d4602959965
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2024-06-04 21:48:02 -0700

    [RF-162] init, Вебпак, Еслинт, Претиер

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">yea-market</header>
+    </div>
+  )
+}
+
+export default App
